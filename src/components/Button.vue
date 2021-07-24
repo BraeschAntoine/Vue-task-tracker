689c80ce542602d441ed074715875c9e749eccb6
Antoine <antoine.braesch@viacesi.fr>
2021-07-24 18:37:42 +0200

    starting to code tasks components

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+    <button @click=onClick() :style='{ background: color}' class='btn'> {{ text }}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text : {
+            type : String, 
+            default: 'Button Component'
+        },
+        color: String
+    },
+    methods: {
+        onClick() {
+            console.log('click')
+        }
+    }
+
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
